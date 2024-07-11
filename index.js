a84a7c3606059779090df09ad899780e3ff09c75
ahmadshaar723 <ahmadshaar723@gmail.com>
2024-07-11 12:55:59 +0300

    ready to deploy

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://mern1-back-1.onrender.com:${port}/images/${req.file.filename}`,
+    image_url: `https://mern1-back-1.onrender.com/images/${req.file.filename}`,
   });
 });
 
